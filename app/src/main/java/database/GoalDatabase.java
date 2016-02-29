0b750951f2b2f8ff07022ab693c962474c858f08
chrisNt <chris.dushi.cs@gmail.com>
2016-02-29 20:35:28 +0200

    Database creation

---
+++ app/src/main/java/database/GoalDatabase.java
@@ -0,0 +1,32 @@
+package database;
+
+import java.util.ArrayList;
+
+import model.Goal;
+
+/**
+ * Created by Chris on 2/29/2016.
+ */
+public class GoalDatabase {
+
+    private static GoalDatabase goalDatabase;
+    private ArrayList<Goal> goals;
+
+
+    public static GoalDatabase newInstance() {
+        if(goalDatabase == null) {
+            goalDatabase = new GoalDatabase();
+        }
+        return goalDatabase;
+    }
+
+    private GoalDatabase() {
+        goals = new ArrayList<>();
+        goals.add(new Goal("Test","test",false));
+    }
+
+    public ArrayList<Goal> getGoals() {
+        return goals;
+    }
+    
+}
