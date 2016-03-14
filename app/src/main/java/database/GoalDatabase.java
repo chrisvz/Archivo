986e56ff8cab6904c2982764ab8d110844f4ce15
chrisNt <chris.dushi.cs@gmail.com>
2016-03-14 22:09:34 +0200

    new methods addGoal and updateGoal

--- app/src/main/java/database/GoalDatabase.java
+++ app/src/main/java/database/GoalDatabase.java
@@ -1,5 +1,6 @@
 package database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -7,6 +8,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import model.Goal;
+
+import static database.DbSchema.*;
 
 /**
  * Created by Chris on 2/29/2016.
@@ -36,9 +39,7 @@ public class GoalDatabase {
         return goalDatabase;
     }
 
-    public void addGoal(Goal g){
-        goals.add(g);
-    }
+
 
     private GoalDatabase(Context context) {
         mContext = context;
@@ -51,5 +52,29 @@ public class GoalDatabase {
 
     public void delete(Goal goal) {
         goals.remove(goal);
+    }
+
+
+
+    private static ContentValues contentValues(Goal goal) {
+
+        ContentValues values = new ContentValues();
+        values.put(GoalBase.COLS.UUID,goal.getUuid().toString());
+        values.put(GoalBase.COLS.TITLE,goal.getTitle());
+        values.put(GoalBase.COLS.DESCRIPTION,goal.getDescription());
+        values.put(GoalBase.COLS.SUCCESSFUL, goal.isSuccessful() ? 1 : 0);
+        return values;
+    }
+
+    public void addGoal(Goal g){
+        ContentValues contentValues = contentValues(g);
+        mDatabase.insert(GoalBase.NAME, null, contentValues);
+        goals.add(g);
+    }
+
+    public void updateGoal(Goal g){
+        String uuid = g.getUuid().toString();
+        ContentValues contentValues = contentValues(g);
+        mDatabase.update(GoalBase.NAME, contentValues , GoalBase.COLS.UUID+" = ?",new String[]{ uuid});
     }
 }
