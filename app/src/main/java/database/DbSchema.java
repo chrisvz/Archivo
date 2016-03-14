8e73c808fe946c117d54a6c07eccbc7fff1498b6
chrisNt <chris.dushi.cs@gmail.com>
2016-03-14 21:40:48 +0200

    Database schema

---
+++ app/src/main/java/database/DbSchema.java
@@ -0,0 +1,16 @@
+package database;
+
+/**
+ * Created by Chris on 3/14/2016.
+ */
+public class DbSchema {
+
+    public static final class GoalDatabase {
+
+        public static final class COLS {
+            public static final String UUID = "uuid";
+            public static final String DESCRIPTION = "description";
+            public static final String SUCCESSFUL = "successful";
+        }
+    }
+}
